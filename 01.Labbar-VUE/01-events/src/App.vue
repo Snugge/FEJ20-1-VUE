4e47bf6de4d0d4d3e6ddee0afb79487c55aaeef8
Mike Blom <blomorama@gmail.com>
2020-11-25 15:19:54 +0100

    Labb1

---
+++ 01.Labbar-VUE/01-events/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <Hero btnText="Klicka Här!"/>
+    <Footer />
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Footer from './components/Footer'
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      
+    }
+  },
+  props: {
+
+  },
+  components: {
+    Navbar,
+    Hero,
+    Footer,
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ 01.Labbar-VUE/01-events/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="my-5">
+		<!--Section: Content-->
+		<section class="px-md-5 py-5 text-center white-text elegant-color ">
+			<h3 class="">Made with <i class="fas fa-heart red-text mx-1"></i> by MDB</h3>
+		</section>
+		<!--Section: Content-->
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
